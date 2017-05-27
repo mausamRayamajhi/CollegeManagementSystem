febeca43e79339a5e6a1a282649ea43fc289d9d3
mausamRayamajhi <mausam495@gmail.com>
2017-05-27 12:39:21 +0700

    comment added mausam

--- CollegeManagementSystem/src/LoginPackage/LoginForm2.java
+++ CollegeManagementSystem/src/LoginPackage/LoginForm2.java
@@ -64,6 +64,7 @@ public class LoginForm2 extends JFrame
   public LoginForm2()
   {
 	  // this is logiform2 constructor
+	  //my name is mausam
     function = new AllMethod();
     pref = Preferences.userRoot().node("RememberMe");
     try {
